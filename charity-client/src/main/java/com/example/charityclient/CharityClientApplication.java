8a02b8ce99c6648e3937c1c8eb4c9ce826037906
Victor Rentea <victorrentea@gmail.com>
2019-07-06 01:29:02 +0300

    sync POST or JMS. all working

---
+++ charity-client/src/main/java/com/example/charityclient/CharityClientApplication.java
@@ -0,0 +1,85 @@
+package com.example.charityclient;
+
+import com.netflix.discovery.converters.Auto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class CharityClientApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityClientApplication.class, args);
+	}
+
+}
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+class CharityController {
+	private final RestTemplate restTemplate;
+	private final Source source;
+
+    @GetMapping("charity")
+    public List<String> getAllCharityNames() {
+    	log.info("Sending GET");
+    	return restTemplate.exchange("http://charity-service/charity", HttpMethod.GET,
+				null, new ParameterizedTypeReference<Resources<CharityDto>>() {
+				}).getBody()
+			.getContent()
+			.stream()
+			.map(CharityDto::getName)
+			.collect(Collectors.toList());
+	}
+
+    @PostMapping("charity")
+	public void createCharity(@RequestBody String name) {
+    	log.info("Sending POST");
+//    	source.output().send(MessageBuilder.withPayload(name).build());
+		restTemplate.postForEntity("http://charity-service/charity", new CharityDto(name),
+                String.class);
+	}
+}
+
+@Data
+class CharityDto {
+	private Long id;
+	private String name;
+
+    public CharityDto() {
+    }
+
+    public CharityDto(String name) {
+        this.name = name;
+    }
+}
+
--- charity-service/src/main/java/com/example/charityservice/CharityServiceApplication.java
+++ charity-service/src/main/java/com/example/charityservice/CharityServiceApplication.java
@@ -1,29 +1,98 @@
 package com.example.charityservice;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class CharityServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CharityServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CharityServiceApplication.class, args);
+    }
 
 }
 
 @RefreshScope
 @RestController
 class HelloController {
-	@Value("${greeting}")
-	private String greeting;
+    @Value("${greeting}")
+    private String greeting;
 
-	@GetMapping("/hello")
-	public String greet() {
-		return greeting;
-	}
+    @GetMapping("/hello")
+    public String greet() {
+        return greeting;
+    }
+}
+
+@Component
+@RequiredArgsConstructor
+class InsertDummyData implements CommandLineRunner {
+    private final CharityRepository repo;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        Stream.of("Orphan Help", "Donate Cloths", "Homeless Shelter Volunteer")
+                .map(Charity::new)
+                .forEach(repo::save);
+    }
+}
+
+@RepositoryRestResource(path = "/charity")
+interface CharityRepository extends JpaRepository<Charity, Long> {
+}
+
+@RequiredArgsConstructor
+@MessageEndpoint
+@Slf4j
+class MessageHandler {
+    private final CharityRepository repo;
+
+    @ServiceActivator(inputChannel = Sink.INPUT)
+    public void createCharity(String charityName) {
+        log.info("Creating Charity");
+        repo.save(new Charity(charityName));
+    }
+}
+
+
+@Data
+@Entity
+class Charity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private LocalDateTime creationTime = LocalDateTime.now();
+
+    private Charity() {
+    }
+
+    public Charity(String name) {
+        this.name = name;
+    }
 }

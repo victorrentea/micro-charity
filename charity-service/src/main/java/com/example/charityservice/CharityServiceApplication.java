80794656065f78ad2b6907ac37a9ceb7c7eb455f
Victor Rentea <victorrentea@gmail.com>
2019-07-05 23:32:22 +0300

    Initial commit

---
+++ charity-service/src/main/java/com/example/charityservice/CharityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.charityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityServiceApplication.class, args);
+	}
+
+}
---
+++ charity-service/src/test/java/com/example/charityservice/CharityServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.charityservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CharityServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

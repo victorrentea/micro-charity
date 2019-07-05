ce821f63d46b579278662411ce827632e5d909cd
Victor Rentea <victorrentea@gmail.com>
2019-07-05 23:56:38 +0300

    refresh works

--- charity-service/src/main/java/com/example/charityservice/CharityServiceApplication.java
+++ charity-service/src/main/java/com/example/charityservice/CharityServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.charityservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CharityServiceApplication {
@@ -10,4 +14,16 @@ public class CharityServiceApplication {
 		SpringApplication.run(CharityServiceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class HelloController {
+	@Value("${greeting}")
+	private String greeting;
+
+	@GetMapping("/hello")
+	public String greet() {
+		return greeting;
+	}
 }
